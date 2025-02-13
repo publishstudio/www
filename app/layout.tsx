a4f70536c5ae8a3151176a93a22f9f6061a7a700
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2025-02-13 10:56:43 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@
 import { Inter } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ScrollToTop from "@/components/ScrollToTop";
+import { Banner } from "@/components/ui/Banner";
+import { siteConfig } from "@/config/site";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,22 +28,22 @@ export default function RootLayout({
       <body
         className={`flex h-screen flex-col items-center justify-center gap-8 bg-[#FCFCFC] dark:bg-black ${inter.className}`}
       >
-        {/* <Providers>
+        <Providers>
           {siteConfig.showBanner && <Banner />}
-          <Header />
+          {/* <Header />
           {children}
           <Footer />
-          <ScrollToTop />
-        </Providers> */}
-        <h1 className="text-center text-7xl font-bold">
-          A new Publish Studio experience is coming soon.
-        </h1>
-        <a
-          href="https://app.publishstudio.one/login"
-          className="text-blue-500 underline"
-        >
-          Old user? Login.
-        </a>
+          <ScrollToTop /> */}
+          <h1 className="text-center text-7xl font-bold">
+            A new Publish Studio experience is coming soon.
+          </h1>
+          <a
+            href="https://app.publishstudio.one/login"
+            className="text-blue-500 underline"
+          >
+            Old user? Login.
+          </a>
+        </Providers>
       </body>
     </html>
   );
